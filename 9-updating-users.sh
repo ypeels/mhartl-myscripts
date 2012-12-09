c0597ae050b5c7033aa5def6ea2c9e01449861a1
Jonathan Chen <ypeels@live.com>
2012-12-09 14:11:23 -0600

    Chapter 9 - first non-trivial commands around Listing 9.30

---
+++ 9-updating-users.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "The amount of stuff Git can't track is decreasing."
+echo "After Listing 9.30, run the following:"
+echo "bundle exec rake db:reset"
+echo "bundle exec rake db:populate # slooow - creates 100 fake users in development env"
+echo "bundle exec rake db:test:prepare"
