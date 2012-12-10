4f10adcd91bc3e7bb5a8bdc17de44b99f4508ce0
Jonathan Chen <ypeels@live.com>
2012-12-09 21:08:40 -0600

    Through Section 10.3.3

--- 10-user-microposts.sh
+++ 10-user-microposts.sh
@@ -7,4 +7,14 @@ if [ ! -e app/models/micropost.rb ]; then
 	echo "tests should pass after that"
 fi
 
+echo 
+echo "After Listing 10.23 run:"
+echo "'bundle exec rake db:reset'"
+echo "'bundle exec rake db:populate' (SLOW!)"
+echo "'bundle exec rake db:test:prepare'"
+
+echo
+echo "Before Listing 10.29"
+echo "rails generate integration_test micropost_pages"
+
 popd
